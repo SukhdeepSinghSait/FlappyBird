165e9bf268c514a997c50c783f156a696f14bc9a
Sukhdeep Singh <Sukhdeep.Singh02@edu.sait.ca>
2024-10-26 14:34:07 -0600

    Animated pipes and they move infinitely now

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 import { useWindowDimensions } from "react-native";
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
-
-
+import { 
+    useSharedValue, 
+    withTiming, 
+    Easing, 
+    withSequence,
+    withRepeat } from "react-native-reanimated";
+import { useEffect } from "react";
 const Index = () => {
     const { width, height } = useWindowDimensions();
 
@@ -11,22 +16,60 @@ const Index = () => {
     const pipeTop = useImage(require('../assets/sprites/pipe-green-top.png'));
     const base = useImage(require('../assets/sprites/base.png'));
 
+    const x = useSharedValue(width-50);  
+    useEffect(() => {
+        x.value = withRepeat( withSequence( withTiming(-150, {duration: 3000, easing: Easing.linear})
+        , withTiming(width, {duration: 0})
+    ), -1
+    );
+    },[]);
+
     const pipeOffset = 0;
 
   return (
     <Canvas style={{ width, height }}>
         {/* Background */}
-        <Image image={bg} width={width} height={height} fit={'cover'} />
+        <Image 
+            image={bg} 
+            width={width} 
+            height={height} 
+            fit={'cover'} 
+        />
 
         {/* Pipe */}
-        <Image image={pipeTop} y={pipeOffset - 360} x={ width/2 } width={104} height={640} />
-        <Image image={pipeDown} y={height - 280 + pipeOffset} x={ width/2 } width={104} height={640} />
+        <Image 
+            image={pipeTop} 
+            y={pipeOffset - 360} 
+            x={ x } 
+            width={104} 
+            height={640} 
+        />
+        <Image 
+            image={pipeDown} 
+            y={height - 280 + pipeOffset} 
+            x={ x } 
+            width={104} 
+            height={640} 
+        />
 
         {/* Base */}
-        <Image image={base} y={height - 75} width={width} height={150} x={0} fit={"cover"} />
+        <Image 
+            image={base} 
+            y={height - 75} 
+            width={width} 
+            height={150} 
+            x={0} 
+            fit={"cover"} 
+        />
         
         {/* Bird */}
-        <Image image={bird} y={height / 2 - 24} x={ width / 4 } width={64} height={48} />
+        <Image 
+            image={bird} 
+            y={height / 2 - 24} 
+            x={ width / 4 } 
+            width={64} 
+            height={48} 
+        />
     </Canvas>
   );
 };
