b3ea8c781f70f62c50ae351f413a88c4a868a887
Sukhdeep Singh <Sukhdeep.Singh02@edu.sait.ca>
2024-10-26 16:10:14 -0600

    Bird physics words

--- app/index.js
+++ app/index.js
@@ -5,8 +5,16 @@ import {
     withTiming, 
     Easing, 
     withSequence,
-    withRepeat } from "react-native-reanimated";
+    withRepeat, 
+    useFrameCallback
+    } from "react-native-reanimated";
 import { useEffect } from "react";
+import { GestureHandlerRootView, 
+    GestureDetector, 
+    Gesture } from 'react-native-gesture-handler';
+
+const GRAVITY = 500;
+
 const Index = () => {
     const { width, height } = useWindowDimensions();
 
@@ -16,7 +24,19 @@ const Index = () => {
     const pipeTop = useImage(require('../assets/sprites/pipe-green-top.png'));
     const base = useImage(require('../assets/sprites/base.png'));
 
-    const x = useSharedValue(width-50);  
+    const x = useSharedValue(width);  
+
+    const birdY = useSharedValue(0);
+    const birdYVelocity = useSharedValue(100);
+
+    useFrameCallback(({ timeSincePreviousFrame: dt}) => {
+        if(!dt){
+            return;
+        }
+        birdY.value = birdY.value + ( birdYVelocity.value * dt ) / 1000;
+        birdYVelocity.value = birdYVelocity.value + ( GRAVITY * dt ) / 1000;
+    });
+
     useEffect(() => {
         x.value = withRepeat( withSequence( withTiming(-150, {duration: 3000, easing: Easing.linear})
         , withTiming(width, {duration: 0})
@@ -24,10 +44,17 @@ const Index = () => {
     );
     },[]);
 
+    const gesture = Gesture.Tap().onStart(() => {
+        birdYVelocity.value = -300
+    });
+
     const pipeOffset = 0;
 
   return (
-    <Canvas style={{ width, height }}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureDetector gesture={gesture}>
+    <Canvas 
+        style={{ width, height }}>
         {/* Background */}
         <Image 
             image={bg} 
@@ -65,12 +92,14 @@ const Index = () => {
         {/* Bird */}
         <Image 
             image={bird} 
-            y={height / 2 - 24} 
+            y={birdY} 
             x={ width / 4 } 
             width={64} 
             height={48} 
         />
     </Canvas>
+    </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
 export default Index;
