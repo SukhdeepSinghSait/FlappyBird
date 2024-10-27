bf680b9c1888810942261b82d6dacef5dcc3f853
Sukhdeep Singh <Sukhdeep.Singh02@edu.sait.ca>
2024-10-27 16:38:42 -0600

    Score system implemented

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import { useWindowDimensions } from "react-native";
-import { Canvas, useImage, Image, Group } from "@shopify/react-native-skia";
+import { 
+    Canvas, 
+    useImage, 
+    Image, 
+    Group,
+    Text,
+    useFont,
+    } from "@shopify/react-native-skia";
 import { 
     useSharedValue, 
     withTiming, 
@@ -10,8 +17,10 @@ import {
     useDerivedValue,
     interpolate,
     Extrapolation,
+    useAnimatedReaction,
+    runOnJS,
     } from "react-native-reanimated";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GestureHandlerRootView, 
     GestureDetector, 
     Gesture } from 'react-native-gesture-handler';
@@ -21,6 +30,7 @@ const JUMP_FORCE = -500;
 
 const Index = () => {
     const { width, height } = useWindowDimensions();
+    const [ score, setScore ] = useState(0);
 
     const bg = useImage(require('../assets/sprites/background-day.png'));
     const bird = useImage(require('../assets/sprites/bluebird-midflap.png'));
@@ -28,17 +38,38 @@ const Index = () => {
     const pipeTop = useImage(require('../assets/sprites/pipe-green-top.png'));
     const base = useImage(require('../assets/sprites/base.png'));
 
+    const font = useFont(require('../assets/fonts/SpaceMono-Regular.ttf'), 30);
+
     const x = useSharedValue(width);  
 
     const birdY = useSharedValue(height / 3);
+    const birdPosition = {
+        x: width / 4,
+    }
     const birdYVelocity = useSharedValue(0);
-    const birdTransform = useDerivedValue(() => {
-        return [{rotate: interpolate(birdYVelocity.value, [-500, 500], [-0.5, 0.5], Extrapolation.CLAMP)}];
-    });
-    const birdOrigin = useDerivedValue(() => {
-        return { x: width/4+32, y: birdY.value + 24};
-    });
 
+    useEffect(() => {
+        x.value = withRepeat( withSequence( withTiming(-150, {duration: 3000, easing: Easing.linear})
+        , withTiming(width, {duration: 0})
+    ), -1
+    );
+    },[]);
+
+    useAnimatedReaction(
+        () => x.value,
+        (currentValue, previousValue) => {
+            const middle = birdPosition.x;
+          if (
+            currentValue !== previousValue &&
+            previousValue &&
+            currentValue < middle &&
+            previousValue >= middle
+          ) {
+            // do something ✨
+            runOnJS(setScore)(score + 1);
+          }
+        }
+      );
 
     useFrameCallback(({ timeSincePreviousFrame: dt}) => {
         if(!dt){
@@ -48,16 +79,29 @@ const Index = () => {
         birdYVelocity.value = birdYVelocity.value + ( GRAVITY * dt ) / 1000;
     });
 
-    useEffect(() => {
-        x.value = withRepeat( withSequence( withTiming(-150, {duration: 3000, easing: Easing.linear})
-        , withTiming(width, {duration: 0})
-    ), -1
-    );
-    },[]);
-
     const gesture = Gesture.Tap().onStart(() => {
         birdYVelocity.value = JUMP_FORCE;
     });
+
+    const birdTransform = useDerivedValue(() => {
+        return [
+            {
+            rotate: interpolate(
+                birdYVelocity.value, 
+                [-500, 500], 
+                [-0.5, 0.5], 
+                Extrapolation.CLAMP
+                ),
+            },
+        ];
+    });
+
+    const birdOrigin = useDerivedValue(() => {
+        return { x: width/4+32, y: birdY.value + 24};
+    });
+
+
+
 
     const pipeOffset = 0;
 
@@ -107,11 +151,20 @@ const Index = () => {
             <Image 
                 image={bird} 
                 y={birdY} 
-                x={ width / 4 } 
+                x={ birdPosition.x } 
                 width={64} 
                 height={48} 
             />
         </Group>
+        {/* Score */}
+        {font && ( // Ensure the font is loaded before rendering the text
+            <Text 
+                x={width / 2}
+                y={100}
+                text={score.toString()}
+                font={font}
+            />
+        )}
     </Canvas>
     </GestureDetector>
     </GestureHandlerRootView>
